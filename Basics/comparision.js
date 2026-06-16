e941e8c0052f838ee773f02f13a400480aa28510
Nishant Kumar <nishant201kashyap@gmail.com>
2026-06-16 08:57:17 +0000

    Comparisions

---
+++ Basics/comparision.js
@@ -0,0 +1,28 @@
+//Two different datatype value comparision
+
+console.log("2">1);
+console.log("02">1);
+
+//Comparisions including converion
+console.log(null>0);
+console.log(null==0);
+console.log(null<=0);
+
+//== & <=,>=,<,> works totally different here as <= >here is doing typeconversion also.
+//null becomes 0 due to type conversion.
+
+
+console.log(undefined>0);
+console.log(undefined==0);
+console.log(undefined<=0);
+//undefined do not behave like a null
+
+//=== strictly check
+console.log("2"===2);
+
+
+
+
+
+
+
