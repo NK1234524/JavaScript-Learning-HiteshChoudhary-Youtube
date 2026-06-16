95064f714b970262fc604d259681485a532a9c60
Nishant Kumar <nishant201kashyap@gmail.com>
2026-06-16 08:35:44 +0000

    Postincrement & Preincrement

--- Basics/String_to_number.js
+++ Basics/String_to_number.js
@@ -10,9 +10,14 @@ console.log(+true) //remains true.
 console.log(+"") // false=>"" + 1 so the value will be true.
 
 let greaterNumber =100;
-console.log(++greaterNumber);//prefix increament
-console.log(greaterNumber++);//postfix increament
+console.log(++greaterNumber);//pre increment => greaterNumber increment by 1
+console.log(greaterNumber++);//post increment => greaterNumber not increment it is just a value.
 
+
+let newNum1 = greaterNumber++; //But here greaterNumber =102 (post increment) & graterNumber+1
+let newNum2 = ++newNum1;//here increament in numNum1 & newNum2 
+console.log(newNum1)
+console.log(newNum2)
 
 
 
