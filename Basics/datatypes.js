c68c6daf6375a682fae1672c805ac8604d62729f
Nishant Kumar <nishant201kashyap@gmail.com>
2026-06-16 06:41:21 +0000

    datatypes

---
+++ Basics/datatypes.js
@@ -0,0 +1,27 @@
+"use strict" // Treat all versions of JS  a  newer version of JS.
+
+//alert(3+3) we are using Node.js not Browser
+
+
+//Make the code more readable
+//DATATYPES
+//number => 2 to 2^53
+//bigint
+//string=>" "
+//boolean=>false or true
+//null=>standalone value
+//undefined=>
+//symbol=>unique
+
+
+console.log(typeof "Nishant");
+//String type
+
+console.log(typeof null);
+//object type
+
+console.log(typeof undefined);
+//Undefined type
+
+
+
---
+++ Basics/variables.js
@@ -0,0 +1,11 @@
+const accountId=14453;
+let accountEmail="nishant@company.com";
+var accountPassword ="1223";
+accountcity = "Jaipur";
+
+let accountstate;
+
+//In JS earlier Js work on the basis of scope so to include scope " " so for that we include var.
+//console.log(accountId=122); Assignment to const value not allowed
+
+console.table([accountEmail,accountId,accountPassword,accountPassword,accountstate]);
