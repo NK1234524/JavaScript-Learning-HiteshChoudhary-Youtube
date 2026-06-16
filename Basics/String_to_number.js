a89d542f886a2a0af63d3901af7c9689e971dfe0
Nishant Kumar <nishant201kashyap@gmail.com>
2026-06-16 08:23:21 +0000

    String to Number conversion

---
+++ Basics/String_to_number.js
@@ -0,0 +1,18 @@
+
+//final value is different
+console.log(1+"2")
+console.log("1"+2)
+console.log("1"+2+2)
+
+
+//Type conversion (Imp) Operator precendence
+console.log(+true) //remains true.
+console.log(+"") // false=>"" + 1 so the value will be true.
+
+let greaterNumber =100;
+console.log(++greaterNumber);//prefix increament
+console.log(greaterNumber++);//postfix increament
+
+
+
+
