faf8ac2facc6271d6be57bc091e696dbd7baf02f
Nishant Kumar <nishant201kashyap@gmail.com>
2026-08-10 05:48:47 +0000

    save

--- Function/Function_01.js
+++ Function/Function_01.js
@@ -7,3 +7,22 @@ function SayMyName(){
 
 // SayMyName is for reference but when () comes it becomes function's execution
 SayMyName()
+
+
+//****** return and console */
+
+function Add(num1,num2){    //num1 and num2 are the parameters here
+    return num1+num2  // return is used to store the value and return it 
+    //whereas console.log is only used for reading the output 
+}
+
+console.log(Add(2,4)); // 2,4 are arguments here
+
+// function implementation
+
+function Logg(username){
+    return `${username} is logged in`//String interpolation is used here
+}
+console.log(Logg()); // argument if blank then undefined will come in output.
+console.log(Logg('Nishant kumar'));
+
