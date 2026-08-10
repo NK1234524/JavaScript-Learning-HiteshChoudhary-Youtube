76decb482a54bd09ed57b1a9ec50752c92acdb91
Nishant Kumar <nishant201kashyap@gmail.com>
2026-08-10 05:25:49 +0000

    ds

---
+++ Function_&_methods/Function_01.js
@@ -0,0 +1,9 @@
+//Here the function is the keyword , SayMyName is the function name & () declaration of the function
+// {} is for defining the function
+function SayMyName(){
+    console.log('N');
+    console.log('I');   
+}
+
+// SayMyName is for reference but when () comes it becomes function's execution
+SayMyName()
