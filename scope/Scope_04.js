b8f22349870f59ec3bb18e0c7a50871e37a9af45
Nishant Kumar <nishant201kashyap@gmail.com>
2026-08-17 16:51:15 +0000

    Save

---
+++ scope/Scope_04.js
@@ -0,0 +1,12 @@
+// Immediately Invoked Function Expressions (IIFE)
+
+// Why IIFE and what is IIFE?
+// To immediately invoke the Fn and also IIFE is special technique to prevent the global scope variable polln
+
+(function chai(){
+    console.log("DB CONNECTED");
+    
+})() // So here without of fn use it will be invoked
+// Also 1st () -> for Fn declaration 
+// 2nd () -> for invoking Fn
+
