ac03b2fe7c00107336c090e12c115c8599f8b058
Nishant Kumar <nishant201kashyap@gmail.com>
2026-08-17 16:57:48 +0000

    Save

--- scope/Scope_04.js
+++ scope/Scope_04.js
@@ -4,9 +4,16 @@
 // To immediately invoke the Fn and also IIFE is special technique to prevent the global scope variable polln
 
 (function chai(){
-    console.log("DB CONNECTED");
-    
-})() // So here without of fn use it will be invoked
+    console.log("DB CONNECTED");    
+})();
+// So here without of fn use it will be invoked
 // Also 1st () -> for Fn declaration 
 // 2nd () -> for invoking Fn
+// It is named IIFE
+// Also if we are using IIFE again then we must make sure to make it end by ;
 
+
+(()=>{
+    console.log("DB Connected 2nd stage");
+})() // So Arrow fn i am using to invoke
+// When arrow fn is used then it is called non-named iife
