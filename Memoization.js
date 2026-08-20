e5d767a056676ce46b1f2a77d8905b3acf5ea151
Nishant Kumar <nishant201kashyap@gmail.com>
2026-08-20 05:56:32 +0000

    Save

--- Memoization.js
+++ Memoization.js
@@ -3,41 +3,71 @@
 //time-consuming calculations by saving previous input to something called cache and returning the result from it.
 
 
-const calc = (num)=>{
-    let sum=0;
-    for(let i = 0 ; i<=num ; i++){
-     sum+=i;    
+// const calc = (num)=>{
+//     let sum=0;
+//     for(let i = 0 ; i<=num ; i++){
+//      sum+=i;    
+//     }
+//     return sum;
+// }
+// console.time();
+
+// console.log(calc(5));
+
+
+// console.timeEnd(); // it is taking a sort of time to implement the fn
+
+// // Now here comes Memoization to make the fn implementation more optimized.
+
+// const Memoization=(fun)=>{
+//     let cache ={}
+//     return function(...args){
+//         let n=args[0]
+//         if(n in cache){
+//             console.log("cache");
+//             return cache[n];            
+//         }
+//         else{
+//             console.log("Calculating 1st time");
+//             let result = fun(n)
+//             cache[n] = result
+//             return result            
+//         }
+//     }
+// }
+
+// console.time();
+// const effi = Memoization(calc);
+// console.log(effi(5));
+// console.timeEnd()
+
+//
+
+const multiply=(num)=>{
+    let value=0;
+    for(let i =1 ; i<=num ;i++){
+        value+=i;       
     }
-    return sum;
+    return value;
 }
-console.time();
 
-console.log(calc(5));
-
-
-console.timeEnd(); // it is taking a sort of time to implement the fn
-
-// Now here comes Memoization to make the fn implementation more optimized.
-
-const Memoization=(fun)=>{
-    let cache ={}
+const Memo = (fn)=>{
+    let cache = {}
+     
     return function(...args){
-        let n=args[0]
-        if(n in cache){
-            console.log("cache");
-            return cache[n];            
-        }
-        else{
-            console.log("Calculating 1st time");
-            let result = fun(n)
-            cache[n] = result
-            return result            
-        }
+        let keys =args[0];
+    if(keys in cache){
+        return cache[keys];
     }
+    else{
+        let result = fn(multiply);
+        cache[keys] = result;
+        return result;        
+    }
+  }
 }
 
 console.time();
-const effi = Memoization(calc);
-console.log(effi(5));
+let ans = Memo(multiply)
+console.log(ans(5))
 console.timeEnd()
-
