b7c453bbcb7feefe477e01ae7025c485cdde61c7
Nishant Kumar <nishant201kashyap@gmail.com>
2026-08-20 06:28:44 +0000

    Save

--- Control_flow/CF_01.js
+++ Control_flow/CF_01.js
@@ -8,3 +8,10 @@ if(false){
     //Every code inside the scope of false condition will not run
 }
 
+
+if(2=='2'){
+    console.log("Executed");    
+}
+// here 2=='2' is not equal because here '2' is in inverted commas and 2 is number.
+// But output is True
+// Here True and False these both are conditions over which it is running.
