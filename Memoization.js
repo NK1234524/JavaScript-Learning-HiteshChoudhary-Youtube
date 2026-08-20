2d80550601d6981fb71ff7ccb1b7d36ed25f1050
Nishant Kumar <nishant201kashyap@gmail.com>
2026-08-20 05:23:29 +0000

    Save

---
+++ Memoization.js
@@ -0,0 +1,43 @@
+//Implementing Memoization in JavaScript
+//Memoization is an optimization technique that can be used to reduce
+//time-consuming calculations by saving previous input to something called cache and returning the result from it.
+
+
+const calc = (num)=>{
+    let sum=0;
+    for(let i = 0 ; i<=num ; i++){
+     sum+=i;    
+    }
+    return sum;
+}
+console.time();
+
+console.log(calc(5));
+
+
+console.timeEnd(); // it is taking a sort of time to implement the fn
+
+// Now here comes Memoization to make the fn implementation more optimized.
+
+const Memoization=(fun)=>{
+    let cache ={}
+    return function(...args){
+        let n=args[0]
+        if(n in cache){
+            console.log("cache");
+            return cache[n];            
+        }
+        else{
+            console.log("Calculating 1st time");
+            let result = fun(n)
+            cache[n] = result
+            return result            
+        }
+    }
+}
+
+console.time();
+const effi = Memoization(calc);
+console.log(effi(5));
+console.timeEnd()
+
