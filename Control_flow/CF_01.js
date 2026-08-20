b5a249b88589982d2fb33e3a526aef749fc9fce0
Nishant Kumar <nishant201kashyap@gmail.com>
2026-08-20 07:12:31 +0000

    Save

--- Control_flow/CF_01.js
+++ Control_flow/CF_01.js
@@ -15,3 +15,31 @@ if(2=='2'){
 // here 2=='2' is not equal because here '2' is in inverted commas and 2 is number.
 // But output is True
 // Here True and False these both are conditions over which it is running.
+
+let number= 23;
+if(number>30){
+    console.log("Greater than 30");
+}
+else{
+    console.log("Not greater than 30");    
+}
+
+// Accessing values under control flow:
+let play = "Music";
+score = 34
+if(score>20){
+   // let playing = "football";// It is having only local scope 
+   var playing = "Football" // gives no restriction it don't have any scope preference.
+    console.log(`Can play : ${play}`);    
+}
+console.log(playing);// Here global scope will come in the playing as var is used.
+
+
+// Implicite way to write the fn
+
+const value = 44;
+if(value>40) console.log(`Yes it is greater then ${value}`),console.log("Not much greater");
+
+// 
+
+
---
+++ Control_flow/CF_02.js
@@ -0,0 +1,31 @@
+const month = 3;
+
+switch(month){
+    case 1 : 
+        console.log("Jan");
+        break;
+    case 2 :
+        console.log("Feb");
+        break;
+    case 3 :
+        console.log("Mar");
+        break;
+    default :
+    console.log("No more cases");          
+}
+
+
+// so if we need a string
+
+const Month = "November";
+
+switch(Month){
+    case "Jan":
+        console.log("January");
+        break;
+    case "Feb":
+        console.log("Feburary");     
+    default:
+        console.log("No existence");
+                   
+}
